f933e13e5c2ef44a378948602d9512bc763c3fde
Saha12sra <74057039+Saha12sra@users.noreply.github.com>
2021-08-28 09:44:21 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 Webcam.set({
-    height: 330,
+    height: 350,
     width: 350,
 img_format: 'png',
 png_quality: 90
@@ -16,4 +16,30 @@ function take_snapshot(){
     })
 } 
 
-Classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/7QLDV5U54/model.json',modelLoaded);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/7QLDV5U54/model.json',modelLoaded);
+
+function modelLoaded(){
+
+    console.log("Model has loaded.");
+
+}
+
+function check(){
+img=document.getElementById("capture_img");
+    classifier.classify(img,gotResult);
+
+}
+
+function gotResult(error, results){
+if(error){
+    console.error(error);
+
+}
+else{
+console.log(results);
+document.getElementById("result_object_name").innerHTML=results[0].label;
+document.getElementById("result_object_accuracy").innerHTML=(results[0].confidence.toFixed(2)*100);
+}
+
+}
+
